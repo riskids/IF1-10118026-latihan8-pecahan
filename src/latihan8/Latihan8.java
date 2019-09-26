de392367ae6950f366dbe12657e911a17b98a4a9
Riski Dwi <bintangmalamhari@gmail.com>
2019-09-26 12:44:31 +0700

    Add files via upload

---
+++ src/latihan8/Latihan8.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan8;
+
+/**
+ *
+ * @author dwisr
+ * NAMA      : RISKI DWI SABARIYANTO
+ * KELAS     : IF -1 
+ * NIM       : 10118026
+ * Deskripsi : Program untuk belajar variabel pecahan
+ * 
+ */
+public class Latihan8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 12342233.3232442F;
+        d = 1_232_423_323.232323;
+        System.out.println("Float "+f);
+        System.out.println("double "+d);
+    }
+    
+}
